f24e2e39c688e18cf8b6b1843b7de2569b8382f4
Mateus Abdallah <abdallahmateus@gmail.com>
2023-09-02 23:18:55 -0700

    create empty JFrame

--- collegeRegistration/Main.java
+++ collegeRegistration/Main.java
@@ -1,10 +1,47 @@
 package collegeRegistration;
 
-public class Main {
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import javax.swing.*;
 
-	public static void main(String[] args) {
+public class Main implements ActionListener {
+
+	private JFrame frame;
+	private JLabel label1, label2, label3, label4;
+	private JTextField field1, field2;
+	
+	
+	
+	
+	public static void main(String[] args) throws IOException {
+		Main obj = new Main();
+		obj.form();
+
+	}
+	
+	public void form() throws IOException{
+		
+		frame = new JFrame("Registration");
+		frame.setLayout(new GridLayout(6,0));
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		
+		
+		
+		
+		
+		frame.setSize(700,500);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 }
