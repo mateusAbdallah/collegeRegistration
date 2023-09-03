974b3b9af0c3e07c2793b951241ed84406effed9
Mateus Abdallah <abdallahmateus@gmail.com>
2023-09-03 12:20:27 -0700

    set two panels with labels and textFields

--- collegeRegistration/Main.java
+++ collegeRegistration/Main.java
@@ -9,8 +9,8 @@ import javax.swing.*;
 public class Main implements ActionListener {
 
 	private JFrame frame;
-	private JLabel label1, label2, label3, label4;
-	private JTextField field1, field2;
+	private JLabel title, label1, label2, label3;
+	private JTextField field1, field2, field3;
 	
 	
 	
@@ -23,10 +23,37 @@ public class Main implements ActionListener {
 	
 	public void form() throws IOException{
 		
-		frame = new JFrame("Registration");
+		//create the frame
+		frame = new JFrame("College Sign In");
 		frame.setLayout(new GridLayout(6,0));
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		//create panel 1
+		JPanel p1 = new JPanel();
+		p1.setLayout(new GridLayout(1,0));		
+		title = new JLabel("Registration", SwingConstants.CENTER);
+		title.setFont(new Font("Verdana", Font.CENTER_BASELINE, 30));
+		
+		p1.add(title);
+		frame.add(p1);
+		
+		//create panel 2
+		JPanel p2 = new JPanel();
+		p2.setLayout(new GridLayout(3,2));
+		label1 = new JLabel("Full name:");
+		field1 = new JTextField(20);
+		label2 = new JLabel("Student ID:");
+		field2 = new JTextField(20);
+		label3 = new JLabel("Program:");
+		field3 = new JTextField(20);
+		
+		p2.add(label1);
+		p2.add(field1);
+		p2.add(label2);
+		p2.add(field2);
+		p2.add(label3);
+		p2.add(field3);
+		frame.add(p2);
 		
 		
 		
